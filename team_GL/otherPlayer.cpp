60b8fceb8f4c1fe1cdcef1a9ae2fb65f25c788aa
keitaogswr <xevolt1027.kamendam@gmail.com>
2017-02-27 16:53:48 +0900

    追加変更

--- team_GL/otherPlayer.cpp
+++ team_GL/otherPlayer.cpp
@@ -61,25 +61,20 @@ void COtherPlayer::Uninit(void)
 void COtherPlayer::Update(void)
 {
 	Vector3 syncPos;
-	syncPos = CSync::GetPos( m_nId );
-	m_nDirection = CSync::GetDirection( m_nId );
-	//m_nState = CSync::GetScore( m_nId );
+	syncPos = CSync::GetPos(m_nId);
 	/*if( syncPos.x >= 0 && syncPos.y >= 0 && syncPos.z >= 0 )
 	{
-		m_Pos = syncPos;
+	m_Pos = syncPos;
 	}*/
 	m_Pos = syncPos;
+	m_nState = CSync::GetState(m_nId);
+	m_nDirection = CSync::GetDirection(m_nId);
 
-	m_nCntAnim++;
-	if (m_nCntAnim == DRAW_SPEED)
-	{
-		m_nCntAnim = 0;
-		m_nPatternAnim++;
-		if (m_nPatternAnim == WALK_DRAW)
-		{
-			m_nPatternAnim = 0;
-		}
-	}
+	// 状態更新
+	UpdateState();
+
+	// アニメーション更新
+	UpdateAnimation();
 }
 
 void COtherPlayer::Draw(void)
